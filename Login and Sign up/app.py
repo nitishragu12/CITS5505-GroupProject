81cfd2016cbadcc6c110915ac24004572e28a71a
Rajashwi Shrestha <rajashwi22@gmail.com>
2024-05-11 21:32:28 +0800

    Created Sign-up page

--- Login and Sign up/app.py
+++ Login and Sign up/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def login():
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
